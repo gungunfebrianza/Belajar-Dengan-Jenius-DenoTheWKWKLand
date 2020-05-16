95146454aca71b22571b2e77c69874b92eaf9e71
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-16 19:27:15 +0700

    Add Assets

--- src/Example.ts
+++ src/Example.ts
@@ -2,3 +2,5 @@ import { serve } from "https://deno.land/std@0.50.0/http/server.ts";
 for await (const req of serve({ port: 8000 })) {
   req.respond({ body: "Hello World\n" });
 }
+
+  let isCute: boolean = true; 
