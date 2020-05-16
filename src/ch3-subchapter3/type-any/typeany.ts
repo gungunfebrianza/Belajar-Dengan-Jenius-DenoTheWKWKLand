b2d9862f54d17d2a626c8348e12db5739af7388a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-16 15:06:48 +0700

    AddTypeAnyParameter

--- src/ch3-subchapter3/type-any/typeany.ts
+++ src/ch3-subchapter3/type-any/typeany.ts
@@ -1,9 +1,9 @@
-  var x: any = 4;
-  x = "Bisa sebuah string";
-  x = false;
+var x: any = 4;
+x = "Bisa sebuah string";
+x = false;
 
-  let diaCantik: any = true; // inisialisasi dengan boolean
-  console.log(typeof diaCantik);
+let diaCantik: any = true; // inisialisasi dengan boolean
+console.log(typeof diaCantik);
 
-  diaCantik = "benar"; // nilai variabel diubah menjadi string
-  console.log(typeof diaCantik);
+diaCantik = "benar"; // nilai variabel diubah menjadi string
+console.log(typeof diaCantik);
