8bb36ccf4f90c3374295f48f9f28205d7fa3007f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-17 22:07:03 +0700

    AddNamingConvention

---
+++ src/ch3-subchapter4/variable-declaration/naming-convention.ts
@@ -0,0 +1,11 @@
+var deep$tech : string = "deep$tech"
+  console.log(deep$tech);
+
+  var deep_tech : string = "deep_tech"
+  console.log(deep_tech);
+
+  var deeptech2019 : string = "deeptech2019"
+  console.log(deeptech2019);
+
+  var namingConvention : string = "camelCase"
+  console.log(namingConvention);
