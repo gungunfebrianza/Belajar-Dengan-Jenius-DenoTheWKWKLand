5092018d9c760ce466b9bf48f9075a72d38da8e0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-17 22:12:33 +0700

    AddUniqueIdentifier

---
+++ src/ch3-subchapter4/variable-declaration/unique-identifier.ts
@@ -0,0 +1,19 @@
+var π: number = Math.PI;
+console.log(π);
+
+var ಠ_ಠ: string = "ಠ_ಠ";
+console.log(ಠ_ಠ);
+
+var ლ_ಠ益ಠ_ლ: string = "ლ_ಠ益ಠ_ლ";
+console.log(ლ_ಠ益ಠ_ლ);
+
+var 〱〱: string = "〱〱";
+console.log(〱〱);
+
+// berbeda:
+// 〱〱 << 〱〱;
+
+// Roman numerals
+var Ⅳ: number = 4;
+var Ⅴ: number = 5;
+console.log(Ⅳ + Ⅴ); // 9
