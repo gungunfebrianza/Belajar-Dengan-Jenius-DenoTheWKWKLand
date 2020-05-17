ea7fa92982cf70fc6966d06eda42beffdb29d71a
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 06:21:58 +0700

    AddExponentExample

---
+++ src/ch3-subchapter4/operator/exponentiation.ts
@@ -0,0 +1,8 @@
+console.log(2 ** 3); // 8
+console.log(3 ** 2); // 9
+console.log(3 ** 2.5); // 15.588457268119896
+console.log(10 ** -1); // 0.1
+console.log(NaN ** 2); // NaN
+console.log(2 ** (3 ** 2)); // 512
+console.log(2 ** (3 ** 2)); // 512
+console.log((2 ** 3) ** 2); // 64
