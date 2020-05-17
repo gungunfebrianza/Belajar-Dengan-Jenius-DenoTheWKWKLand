b2bde5650c28d2b0c60db723b920734d37c23602
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 06:54:21 +0700

    AddFixCode

--- src/ch3-subchapter4/strict-mode/strict-mode.ts
+++ src/ch3-subchapter4/strict-mode/strict-mode.ts
@@ -1,4 +1,14 @@
-function sloppyFunction() {
-  sloppyVariable = 777
-}
+/*     // Javascript kode akan dieksekusi dengan mode strict
+    'use strict';
 
+    // Error akan terjadi karena x belum dideklarasikan
+    x = 3.14; //ReferenceError: x is not defined
+
+    // Use Strict di deklarasikan didalam sebuah function,
+    // Strict dalam sekup lokal
+    myFunction();
+    function myFunction() {
+      ('use strict');
+      y = 3.14; //Error. ReferenceError: y is not defined
+    }
+ */
