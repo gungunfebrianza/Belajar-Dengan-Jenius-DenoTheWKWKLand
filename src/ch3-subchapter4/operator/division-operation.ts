f17eef4b127a89a7ebec2331e8cceab68421355e
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 06:20:15 +0700

    AddDivisionOperation

---
+++ src/ch3-subchapter4/operator/division-operation.ts
@@ -0,0 +1,5 @@
+console.log(1 / 2); // returns 0.5 in JavaScript
+console.log(1.0 / 2.0); // returns 0.5 in both js & Java
+console.log(2.0 / 0); // returns Infinity in JavaScript
+console.log(2.0 / 0.0); // returns Infinity too
+console.log(2.0 / -0.0); // returns -Infinity in JavaScript
