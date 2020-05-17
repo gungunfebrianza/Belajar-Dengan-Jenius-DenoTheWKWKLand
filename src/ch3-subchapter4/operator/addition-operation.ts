13fdc21d21ce7aef78cf3021c581d24b4930c1b7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 06:18:13 +0700

    AddAdditionOperation

---
+++ src/ch3-subchapter4/operator/addition-operation.ts
@@ -0,0 +1,12 @@
+  //The addition operator produces the sum of numeric operands or string concatenation.
+  // Number + Number -> addition
+  console.log(1 + 2); // 3
+
+  // Number + String -> concatenation
+  console.log(5 + 'foo'); // "5foo"
+
+  // String + Boolean -> concatenation
+  console.log('foo' + false); // "foofalse"
+
+  // String + String -> concatenation
+  console.log('foo' + 'bar'); // "foobar"
