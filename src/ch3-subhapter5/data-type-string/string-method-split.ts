db2b404c32b3b797428c7ee543a791afdd08011f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 11:12:49 +0700

    AddExample

--- src/ch3-subhapter5/data-type-string/string-method-split.ts
+++ src/ch3-subhapter5/data-type-string/string-method-split.ts
@@ -1,7 +1,7 @@
-  const name: string = "Hello Maudy Ayunda";
+const name: string = "Hello Maudy Ayunda";
 
-  const arrayString = name.split(" ");
-  console.log(arrayString[2]); //ayunda
+const arrayString = name.split(" ");
+console.log(arrayString[2]); //ayunda
 
-  const arrayChars = name.split("");
-  console.log(arrayChars[4]); //0
+const arrayChars = name.split("");
+console.log(arrayChars[4]); //0
