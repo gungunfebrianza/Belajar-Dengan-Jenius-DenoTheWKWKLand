0e0f3bd2b92874031117e44c37fa3d0b01e7e9a2
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 17:15:25 +0700

    AddContinue

--- src/ch3-subchapter7/break/break-statement.ts
+++ src/ch3-subchapter7/break/break-statement.ts
@@ -4,7 +4,7 @@ for (i; i < 10; i++) {
   console.log("Perulangan ke " + i);
 }
 /* Output :
-Perulangan ke 0
-Perulangan ke 1
-Perulangan ke 2
-*/
+  Perulangan ke 0
+  Perulangan ke 1
+  Perulangan ke 2
+  */
