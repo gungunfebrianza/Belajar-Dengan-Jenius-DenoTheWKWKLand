4e2f0133c35fb41d2c51f149578ae7b3db5b095d
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 10:56:06 +0700

    Add StringMethod

---
+++ src/ch3-subhapter5/data-type-string/string-method-split.ts
@@ -0,0 +1,7 @@
+  const name: string = "Hello Maudy Ayunda";
+
+  const arrayString = name.split(" ");
+  console.log(arrayString[2]); //ayunda
+
+  const arrayChars = name.split("");
+  console.log(arrayChars[4]); //0
