64137a4c8512673ffaa128222e1536a005301031
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 11:31:13 +0700

    UpdateENotation

--- src/ch3-subhapter5/data-type-number/eNotation.ts
+++ src/ch3-subhapter5/data-type-number/eNotation.ts
@@ -2,3 +2,11 @@ let billion: number = 1e9; //1 billion (1 & 9 zero)
 //Contoh lainya :
 // 1e3 = 1*1000
 // 1.23e6 = 1.23 * 1000000
+
+//explicit zero
+let ms: number = 0.000001;
+//implicit zero
+ms = 1e-6;
+
+//1e-3 = 1 / 1000
+//1.23e-6 = 1.23 / 1000000
