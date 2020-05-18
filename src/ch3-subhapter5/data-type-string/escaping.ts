51722c070cc4b24a6349ee48e98240b72088118b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 08:03:50 +0700

    AddEscapeSequence

---
+++ src/ch3-subhapter5/data-type-string/escaping.ts
@@ -0,0 +1,5 @@
+console.log("Maudy \n Ayunda \n Faza");
+console.log('Maudy " Ayunda " Faza');
+console.log("Maudy \' Ayunda \' Faza");
+console.log("Maudy \t Ayunda \t Faza");
+console.log("\xA9");
