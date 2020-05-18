3c68bbb4caff68445bbdcd53043743f59520d026
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 17:07:05 +0700

    AddForStatement

---
+++ src/ch3-subchapter7/for/for-decrement.ts
@@ -0,0 +1,12 @@
+let index2: number = 10;
+for (index2; index > 5; index2--) {
+  console.log("Perulangan ke " + index2);
+}
+
+/* Output :
+Perulangan ke 10
+Perulangan ke 9
+Perulangan ke 8
+Perulangan ke 7
+Perulangan ke 6
+*/
--- src/ch3-subchapter7/for/for-statement.ts
+++ src/ch3-subchapter7/for/for-statement.ts
@@ -3,9 +3,9 @@ for (index; index < 5; index++) {
   console.log("Perulangan ke " + index);
 }
 /* Output :
-Perulangan ke 0
-Perulangan ke 1
-Perulangan ke 2
-Perulangan ke 3
-Perulangan ke 4
-*/
+  Perulangan ke 0
+  Perulangan ke 1
+  Perulangan ke 2
+  Perulangan ke 3
+  Perulangan ke 4
+  */
