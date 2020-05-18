c3f96b4996b75ca240561c2409814e94e0c5e36b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 14:45:59 +0700

    AddOptionalProperty

---
+++ src/ch3-subhapter5/data-type-custom/optional-property.ts
@@ -0,0 +1,11 @@
+  type CryptoWallet = {
+    name: string;
+    amount: Balance;
+    symbol?: Type;
+  };
+
+  let ethereumWallet: CryptoWallet = {
+    name: "Ethereum",
+    amount: 23.88800007,
+    symbol: "ETH",
+  };
