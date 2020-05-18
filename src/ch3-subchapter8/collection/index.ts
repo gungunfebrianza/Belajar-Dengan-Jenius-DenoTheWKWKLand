4b2d7247c88667110203123015f1b8666a13f9f4
restuwahyu13 <restuwahyu13@gmail.com>
2020-05-19 05:08:28 +0700

    Create index.ts

---
+++ src/ch3-subchapter8/collection/index.ts
@@ -0,0 +1,7 @@
+//  collection using MAP constructor
+const numb = new Map<string, number>([["satu", 1], ["dua", 2], ["tiga", 3]])
+console.log(numb);
+
+// collection menggunakan SET constructor
+const days = new Set <string> (["senin", "selasa", "rabu"]);
+console.log(days);
