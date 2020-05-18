b02d1e26a803ffa3a32ba8828bf33731b629fed8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 15:20:04 +0700

    AddMultiConditionalStatement

---
+++ src/ch3-subchapter6/control-flow/if-else-statement.ts
@@ -0,0 +1,11 @@
+let time: number = 44;
+let x: string;
+if (time < 20) {
+  x = "Statement_1";
+} else if (time == 20) {
+  x = "Statement_2";
+} else {
+  x = "Statement_3";
+}
+//output
+//"Statement_3"
