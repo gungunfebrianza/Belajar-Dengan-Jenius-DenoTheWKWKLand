5102b78794ec074544c0c811f957de8c8a68d061
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-18 07:23:00 +0700

    AddExample

---
+++ src/ch3-subchapter4/clean-code/declare-on-top.ts
@@ -0,0 +1,12 @@
+var firstName, lastName: string;
+var price, discount, fullPrice: number;
+
+firstName = "Maudy";
+lastName = "Ayunda";
+
+price = 136.36;
+discount = 0.10;
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullPrice = price * 100 / discount);
