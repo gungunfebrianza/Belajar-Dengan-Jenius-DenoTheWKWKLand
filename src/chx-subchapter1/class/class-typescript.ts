942cb47b8ce55304016f94330a6a0828066dda33
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-19 11:50:15 +0700

    Add Asset ClassDeclaration

--- src/chx-subchapter1/class/class-typescript.ts
+++ src/chx-subchapter1/class/class-typescript.ts
@@ -4,7 +4,14 @@ class Person {
   age = 0;
   eyecolor = "";
 
-  getName(): string {
+  getFullName(): string {
     return this.firstName + " " + this.lastName;
   }
 }
+
+  const hooman = new Person();
+  hooman.firstName = "Maudy";
+  hooman.lastName = "Ayunda";
+  hooman.age = 25;
+  hooman.eyecolor = "Brown";
+  console.log(hooman.getFullName());
