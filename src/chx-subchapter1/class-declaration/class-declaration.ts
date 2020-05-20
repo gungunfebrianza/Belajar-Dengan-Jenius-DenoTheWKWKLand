c03448374415926be81e338994b23a88b7ccefc8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-20 14:33:54 +0700

    UpdateScript

--- src/chx-subchapter1/class-declaration/class-declaration.ts
+++ src/chx-subchapter1/class-declaration/class-declaration.ts
@@ -1,6 +1,9 @@
-  class Wallet {
-    constructor(public id: number, public balance: number) {
-      this.id = id;
-      this.balance = balance;
-    }
+class Wallet {
+  constructor(public id: number, public balance: number) {
+    this.id = id;
+    this.balance = balance;
   }
+}
+
+let myWallet = new Wallet(1, 2000);
+console.log(myWallet);
