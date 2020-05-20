5cb549649837d34b4fa03dbfaa21fa90371485c8
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-20 15:07:28 +0700

    DeleteExample

---
+++ src/chx-subchapter1/class-expression/class-unamed.ts
@@ -0,0 +1,9 @@
+let mywallet1 = class Wallet {
+  constructor(public id: number, public balance: number) {
+    this.id = id;
+    this.balance = balance;
+  }
+};
+
+mywallet1 = Wa
+console.log(typeof mywallet1); //function
