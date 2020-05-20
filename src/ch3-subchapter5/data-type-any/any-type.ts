04bea1ec66c229c7208d23014c59211d8d132487
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-20 23:51:50 +0700

    AddUpdate

--- src/ch3-subchapter5/data-type-any/any-type.ts
+++ src/ch3-subchapter5/data-type-any/any-type.ts
@@ -1,3 +1,10 @@
-var x: any = 4;
+var x: any = 4; // Explicitly typed
 x = "Bisa sebuah string";
 x = false;
+
+var y; // Same as y: any
+var z: { a; b }; // Same as z: { a: any; b: any; }
+
+function f(x) { // Same as f(x: any): void
+  console.log(x);
+}
