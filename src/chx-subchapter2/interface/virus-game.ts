e75789ad72919c66d7a4e31bf5a8a65f813b6ec6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-21 15:27:07 +0700

    Add Asset InterfaceMalware

--- src/chx-subchapter2/interface/virus-game.ts
+++ src/chx-subchapter2/interface/virus-game.ts
@@ -4,18 +4,6 @@ interface Malware {
   infectStartup(): void;
 }
 
-interface Trojan {
-  TrojanVersion: string;
-  keylogger(): void;
-  screenlogger(): void;
-}
-
-interface Ransomware {
-  RansomwareVersion: string;
-  encrypt(): void;
-  extortion(): void;
-}
-
 class Virus implements Malware {
   public IDMalware: string;
   constructor(IDmalware: string) {
@@ -30,3 +18,37 @@ class Virus implements Malware {
 }
 
 const vir1 = new Virus("1");
+
+interface Trojan extends Malware {
+  TrojanVersion: string;
+  keylogger(): void;
+  screenlogger(): void;
+}
+
+class virus2 implements Trojan {
+  public TrojanVersion: string;
+  keylogger(): void {
+    console.log("Keylogger Active");
+  }
+  screenlogger(): void {
+    console.log("Screenlogger Active");
+  }
+  public IDMalware: string;
+  constructor(IDmalware: string) {
+    this.IDMalware = IDmalware;
+  }
+  getOSInformation() {
+    console.log(`OS Windows User Maudy`);
+  }
+  infectStartup() {
+    console.log();
+  }
+}
+
+
+
+interface Ransomware {
+  RansomwareVersion: string;
+  encrypt(): void;
+  extortion(): void;
+}
