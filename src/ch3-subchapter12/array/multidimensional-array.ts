0162a2def866456c2173db1c283792881bda25d4
Arifin Izzah <arifin.izzah15@gmail.com>
2020-05-21 16:18:55 +0700

    add multidimensional array

---
+++ src/ch3-subchapter12/array/multidimensional-array.ts
@@ -0,0 +1,7 @@
+var multi = [[1,2,3],[23,24,25]]
+console.log(multi[0][0])
+console.log(multi[0][1])
+console.log(multi[0][2])
+console.log(multi[1][0])
+console.log(multi[1][1])
+console.log(multi[1][2])
