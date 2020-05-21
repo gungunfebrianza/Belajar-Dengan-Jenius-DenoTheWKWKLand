5c86f8954b73fb3127642db8db2397e66fee7afa
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-22 00:46:13 +0700

    Add Asset Collection Type

--- src/ch3-subchapter5/data-type-number/accuration.ts
+++ src/ch3-subchapter5/data-type-number/accuration.ts
@@ -1,7 +1,7 @@
-var x: number = 999999999999999; // x will be 999999999999999
-var y: number = 9999999999999999; // y will be 10000000000000000
+  var x: number = 999999999999999;
+  var y: number = 9999999999999999;
 
-console.log(x);
-//999999999999999
-console.log(y);
-//10000000000000000
+  console.log(x);
+  //999999999999999
+  console.log(y);
+  //10000000000000000
