197ed8afaba07ed47154fcbad05dcf92521fa7a6
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-21 23:04:56 +0700

    Fix Asset Interface

--- src/chx-subchapter2/interface/virus-game1.ts
+++ src/chx-subchapter2/interface/virus-game1.ts
@@ -4,6 +4,12 @@ interface Malware {
   infectStartup(): void;
 }
 
+interface Trojan {
+  IDTrojan: string;
+  keylogger(): void;
+  screenlogger(): void;
+}
+
 interface Ransomware extends Malware {
   RansomwareVersion: string;
   encrypt(): void;
@@ -31,21 +37,21 @@ exampleVirus1.getOSInformation();
 exampleVirus1.infectStartup();
 exampleVirus1.getIDMalware();
 
-  class WannaCry implements Ransomware {
-    public RansomwareVersion: string;
-    public IDMalware: string;
-    getOSInformation(): void {
-      console.log("OS Windows : User Maudy");
-    }
-    infectStartup(): void {
-      console.log("Startup OS Infected");
-    }
-    encrypt(): void {
-      console.log("Encrypt All Files, Success");
-    }
-    extortion(): void {
-      console.log("Send Bitcoin to : 8sdhshj6sgjhd&");
-    }
+class WannaCry implements Ransomware {
+  public RansomwareVersion: string;
+  public IDMalware: string;
+  getOSInformation(): void {
+    console.log("OS Windows : User Maudy");
   }
+  infectStartup(): void {
+    console.log("Startup OS Infected");
+  }
+  encrypt(): void {
+    console.log("Encrypt All Files, Success");
+  }
+  extortion(): void {
+    console.log("Send Bitcoin to : 8sdhshj6sgjhd&");
+  }
+}
 
 const exampleVirus3: WannaCry = new WannaCry();
