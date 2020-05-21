3078e2b2a851d08b232abe744cde0d43a7cc6416
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-22 00:39:26 +0700

    AddAccuration

--- src/ch3-subchapter5/data-type-number/accuration.ts
+++ src/ch3-subchapter5/data-type-number/accuration.ts
@@ -1,8 +1,7 @@
-  console.log(typeof undefined);
-  //"undefined"
-  console.log(typeof null);
-  //"object"
-  console.log(null === undefined);
-  //false
-  console.log(null === undefined);
-  //true
+var x = 999999999999999; // x will be 999999999999999
+var y = 9999999999999999; // y will be 10000000000000000
+
+console.log(x);
+//999999999999999
+console.log(y);
+//10000000000000000
