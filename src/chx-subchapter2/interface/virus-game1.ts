58321db06a898ff103f195c0f0dc87525ac58c6c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-21 21:47:02 +0700

    Fix Asset InterfaceContract1

--- src/chx-subchapter2/interface/virus-game1.ts
+++ src/chx-subchapter2/interface/virus-game1.ts
@@ -4,11 +4,11 @@ interface Malware {
   infectStartup(): void;
 }
 
-  interface Ransomware extends Malware {
-    RansomwareVersion: string;
-    encrypt(): void;
-    extortion(): void;
-  }
+interface Ransomware extends Malware {
+  RansomwareVersion: string;
+  encrypt(): void;
+  extortion(): void;
+}
 
 class Virus implements Malware {
   public IDMalware: string;
@@ -30,26 +30,22 @@ const exampleVirus1: Virus = new Virus("101");
 exampleVirus1.getOSInformation();
 exampleVirus1.infectStartup();
 exampleVirus1.getIDMalware();
-console.log(typeof exampleVirus1);
 
-const exampleVirus2: Malware = new Virus("102");
-console.log(typeof exampleVirus2);
-
-class WannaCry implements Ransomware {
-  public RansomwareVersion: string;
-  public IDMalware: string;
-  getOSInformation(): void {
-    console.log("OS Windows : User Maudy");
+  class WannaCry implements Ransomware {
+    public RansomwareVersion: string;
+    public IDMalware: string;
+    getOSInformation(): void {
+      console.log("OS Windows : User Maudy");
+    }
+    infectStartup(): void {
+      console.log("Startup OS Infected");
+    }
+    encrypt(): void {
+      console.log("Encrypt All Files, Success");
+    }
+    extortion(): void {
+      console.log("Send Bitcoin to : 8sdhshj6sgjhd&");
+    }
   }
-  infectStartup(): void {
-    console.log("Startup OS Infected");
-  }
-  encrypt(): void {
-    console.log("Encrypt All Files, Success");
-  }
-  extortion(): void {
-    console.log("Send Bitcoin to : 8sdhshj6sgjhd&");
-  }
-}
 
 const exampleVirus3: WannaCry = new WannaCry();
