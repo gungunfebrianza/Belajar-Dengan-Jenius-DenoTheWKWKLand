3650e1b0de5c38787cfbb8209844cf6b80f12637
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-22 00:03:32 +0700

    UpdateScript

---
+++ src/ch3-subchapter5/data-type-bigInteger/out/big-integer.js
@@ -0,0 +1,7 @@
+const bigInt1 = 1234567890123456789012345678901234567890n;
+const bigInt2 = BigInt("1234567890123456789012345678901234567890");
+const bigInt3 = BigInt(1234567890123456789012345678901234567890);
+console.log(bigInt1);
+console.log(bigInt2);
+console.log(bigInt3);
+//# sourceMappingURL=big-integer.js.map
