92d37dafc15ae95678127256f8ea566698404dd4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-22 00:35:00 +0700

    AddAcurration

---
+++ src/ch3-subchapter5/data-type-number/accuration.ts
@@ -0,0 +1,8 @@
+  console.log(typeof undefined);
+  //"undefined"
+  console.log(typeof null);
+  //"object"
+  console.log(null === undefined);
+  //false
+  console.log(null === undefined);
+  //true
