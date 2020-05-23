f5ce3c925b628317e3daba9020adcf1846fd525b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 14:34:07 +0700

    AddCompilation

---
+++ src/chx-subchapter3/module-typescript/dist/simple-module.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.birthday = exports.getHerSong = exports.getName = void 0;
+exports.getName = () => {
+    return "My Name is Maudy";
+};
+exports.getHerSong = () => {
+    return "Perahu Kertas";
+};
+exports.birthday = "19 December 1994";
--- src/chx-subchapter3/module-typescript/simple-module.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getName = void 0;
-exports.getName = () => {
-    return "My Name is Maudy";
-};
