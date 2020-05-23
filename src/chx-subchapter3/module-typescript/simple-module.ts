c72a9264b1576400161eabff1fec38f511c97cc4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 14:12:02 +0700

    Add Asset ModuleDenoExport

--- src/chx-subchapter3/module-typescript/simple-module.ts
+++ src/chx-subchapter3/module-typescript/simple-module.ts
@@ -1,3 +1,9 @@
   export let getName = (): string => {
     return "My Name is Maudy";
   };
+
+export let getHerSong = (): string => {
+  return "Perahu Kertas";
+};
+
+export const birthday: string = "19 December 1994";
