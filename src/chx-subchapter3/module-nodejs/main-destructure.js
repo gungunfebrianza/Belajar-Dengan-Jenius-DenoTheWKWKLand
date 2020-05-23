98f95d40472b54873aa5dbfce28d07b6ec630b0b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 13:50:20 +0700

    AddMainDestructure

---
+++ src/chx-subchapter3/module-nodejs/main-destructure.js
@@ -0,0 +1,5 @@
+const { birthday, getName } = require("./simple-module");
+console.log(`Maudy birthday is ${birthday}`);
+// Maudy birthday is 19 December 1994
+console.log(getName());
+// Maudy Ayunda
