f8ab91ca2728d7b5b001de14459f35188a80e189
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 16:48:57 +0700

    AddTypeScriptVersion

---
+++ src/ch3-subchapter8/function-parameter/function-parameter.ts
@@ -0,0 +1,11 @@
+function displayMessage(from: string, text: string): void {
+  // arguments: from, text
+  console.log(from + ": " + text);
+}
+
+displayMessage("Kaiz", "Hello! Maudy Ayunda!");
+displayMessage("Maudy Ayunda", "What's up? kaiz!");
+/* 
+  Output
+  Kaiz: Hello! Maudy Ayunda!
+  Maudy Ayunda: What's up? kaiz! */
