db912b11a8741ce9bc05710560b074e622eef3f9
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 17:01:54 +0700

    AddFunctionExample

---
+++ src/ch3-subchapter8/function-outer-variable/function.ts
@@ -0,0 +1,8 @@
+  let userName = "Gun Gun Febrianza";
+
+  function tampilkanPesan(): void {
+    let message = "Hello, " + userName;
+    console.log(message);
+  }
+
+  tampilkanPesan(); // Hello, Gun Gun Febrianza
