b83738f12f18bd664c736021510cf731c30a600b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 16:44:59 +0700

    AddFunctionParameter

---
+++ src/ch3-subchapter8/function-parameter/function-parameter.js
@@ -0,0 +1,11 @@
+function tampilkanPesan(from, text) {
+  // arguments: from, text
+  console.log(from + ": " + text);
+}
+
+tampilkanPesan("Kaiz", "Hello! Maudy Ayunda!");
+tampilkanPesan("Maudy Ayunda", "What's up? kaiz!");
+/* 
+Output
+Kaiz: Hello! Maudy Ayunda!
+Maudy Ayunda: What's up? kaiz! */
