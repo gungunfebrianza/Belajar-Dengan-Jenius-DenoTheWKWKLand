875ca6d12d2451495a72f609aa9fae970bc40658
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 15:13:06 +0700

    UpdateScript

--- src/chx-subchapter3/module-deno/dist/main.js
+++ src/chx-subchapter3/module-deno/dist/main.js
@@ -2,4 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const mod_1 = require("./mod");
 mod_1.readBook();
+// Belajar Dengan Jenius Typescript karya Gun Gun Febrianza
 console.log(mod_1.bookDetail);
+// { author: 'Gun Gun Febrianza', price: 90000 }
+console.log(mod_1.publication);
--- src/chx-subchapter3/module-deno/main.ts
+++ src/chx-subchapter3/module-deno/main.ts
@@ -1,3 +1,7 @@
-import { readBook, bookDetail } from "./mod";
+import { readBook, bookDetail, publication } from "./mod";
 readBook();
+// Belajar Dengan Jenius Typescript karya Gun Gun Febrianza
 console.log(bookDetail);
+// { author: 'Gun Gun Febrianza', price: 90000 }
+console.log(publication);
+// [ 'PinterCoding', 2020 ]
