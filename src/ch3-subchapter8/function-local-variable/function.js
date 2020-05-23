f6f1f740d388ab946ed2b05e2514b57a7bd933de
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 16:58:40 +0700

    AddFunctionScope

---
+++ src/ch3-subchapter8/function-local-variable/function.js
@@ -0,0 +1,8 @@
+function tampilkanPesan() {
+  let message = "Hello, I'm Message Variable Inside Function"; // local
+  console.log(message);
+}
+
+tampilkanPesan();
+console.log(message);
+// <-- Error! ReferenceError: message is not defined
