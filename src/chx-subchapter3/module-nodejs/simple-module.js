fbee3dca0bcd63562659d4508b4bb09ad938e46c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 13:49:14 +0700

    UpdateScript

--- src/chx-subchapter3/module-nodejs/simple-module.js
+++ src/chx-subchapter3/module-nodejs/simple-module.js
@@ -2,4 +2,12 @@ const getName = () => {
   return "Maudy Ayunda";
 };
 
+const getHerSong = () => {
+  return "Perahu Kertas";
+};
+
+const birthday = "19 December 1994";
+
 exports.getName = getName;
+exports.getHerSong = getHerSong;
+exports.birthday = birthday;
