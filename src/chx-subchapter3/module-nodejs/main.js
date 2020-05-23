7fde974c9c7d69a89d64eea2d2cfe646f375cab0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 13:49:30 +0700

    UpdateScript

--- src/chx-subchapter3/module-nodejs/main.js
+++ src/chx-subchapter3/module-nodejs/main.js
@@ -1,3 +1,7 @@
 const angel = require("./simple-module");
 console.log(angel.getName());
 // Maudy Ayunda
+console.log(angel.birthday);
+// 19 December 1994
+console.log(angel.getHerSong());
+// Perahu Kertas
