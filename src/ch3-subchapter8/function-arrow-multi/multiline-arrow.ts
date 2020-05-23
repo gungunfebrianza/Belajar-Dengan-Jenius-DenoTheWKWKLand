62623ed43476a4418755659da2081899df44b72c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-23 17:14:01 +0700

    AddMultilineArrow

---
+++ src/ch3-subchapter8/function-arrow-multi/multiline-arrow.ts
@@ -0,0 +1,7 @@
+let summary = (a: number, b: number): number => {
+  // curly brace untuk membuat multiline function
+  let result = a + b;
+  return result; // gunakan return untuk memproduksi hasil
+};
+
+console.log(summary(1, 2)); // 3
