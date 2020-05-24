0401e7b21b82a1bb3d0711ff44d48216bbf999fa
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-24 22:47:03 +0700

    AddToFixed

--- src/ch3-subchapter5/data-type-number/toFixed.ts
+++ src/ch3-subchapter5/data-type-number/toFixed.ts
@@ -4,3 +4,13 @@ console.log((0.1 + 0.2).toFixed(2));
 //0.30
 console.log(typeof (0.1 + 0.2).toFixed(2));
 //string
+
+let sum = 0.1 + 0.2;
+console.log(sum.toFixed(2));
+//0.3
+
+console.log(typeof sum.toFixed(2));
+//"number"
+
+console.log(typeof sum.toFixed(2));
+//"string"
