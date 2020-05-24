7b39c1278c7742c730789e07789811b705cb748c
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-25 00:45:48 +0700

    UpdateScript

--- src/ch3-subchapter12/array/array-iterator-object.ts
+++ src/ch3-subchapter12/array/array-iterator-object.ts
@@ -1,5 +1,14 @@
-var soul = ["Maudy", "Ayunda Faza", "Gun Gun", "Febrianza"]
-var x soul.entries()
+var soul: string[] = ["Maudy", "Ayunda Faza", "Gun Gun", "Febrianza"];
+var x = soul.entries();
+var n: [number, string];
 for (n of x) {
-    console.log(n=n)
+  console.log(n = n);
 }
+/*
+    [ 0, "Maudy" ]
+    [ 1, "Ayunda Faza" ]
+    [ 2, "Gun Gun" ]
+    [ 3, "Febrianza" ] 
+    */
+console.log(x);
+//Array Iterator {}
--- src/ch3-subchapter12/array/array-looping-es6.ts
+++ src/ch3-subchapter12/array/array-looping-es6.ts
@@ -1,3 +1,9 @@
-mod.forEach(function (item,index, array) {
-    console.log(item, index);
+var mod: string[] = ["Maudy", "Ayunda", "Faza"];
+mod.forEach(function (item: string, index: number) {
+  console.log(item, index);
 });
+/* 
+    Maudy 0
+    Ayunda 1
+    Faza 2 
+    */
