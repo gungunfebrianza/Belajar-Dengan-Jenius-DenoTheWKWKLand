a68aaefe67d4cfe7f48718a5c356d6d9785749ef
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-25 23:59:36 +0700

    AddDeleteItemSet

---
+++ src/ch3-subchapter12/set/delete-item.ts
@@ -0,0 +1,15 @@
+const exampleMap3 = new Map();
+console.log(exampleMap3.set("1", "Maudy"));
+// Map { "1" => "Maudy" }
+console.log(exampleMap3.set(1, "Ayunda"));
+// Map { "1" => "Maudy", 1 => "Ayunda" }
+console.log(exampleMap3.set(true, "Faza"));
+// Map { "1" => "Maudy", 1 => "Ayunda", true => "Faza" }
+
+exampleMap3.delete(true);
+console.log(exampleMap3);
+/* 
+  Maudy
+  Ayunda
+  Faza 
+  */
--- src/ch3-subchapter12/set/get-item.ts
+++ src/ch3-subchapter12/set/get-item.ts
@@ -6,10 +6,10 @@ console.log(exampleMap2.set(1, "Ayunda"));
 console.log(exampleMap2.set(true, "Faza"));
 // Map { "1" => "Maudy", 1 => "Ayunda", true => "Faza" }
 
-  console.log(exampleMap2.get("1"));
-  console.log(exampleMap2.get(1));
-  console.log(exampleMap2.get(true));
-  /* 
+console.log(exampleMap2.get("1"));
+console.log(exampleMap2.get(1));
+console.log(exampleMap2.get(true));
+/* 
   Maudy
   Ayunda
   Faza 
