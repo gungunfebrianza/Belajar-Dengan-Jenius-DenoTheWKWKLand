8f90f18c5ba2ca1e97743ccfcc253aa9860303b5
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-26 00:10:54 +0700

    AddCheckItem

---
+++ src/ch3-subchapter12/set/check-item.ts
@@ -0,0 +1,11 @@
+const exampleMap4 = new Map();
+exampleMap4.set("1", "Maudy");
+exampleMap4.set(1, "Ayunda");
+exampleMap4.set(true, "Faza");
+
+  console.log(exampleMap4.has("1"));
+  console.log(exampleMap4.has("3"));
+  /* 
+  true
+  false
+    */
