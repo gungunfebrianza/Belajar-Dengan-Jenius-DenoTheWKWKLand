11f7547cf320f41027bb7bd1e02870665759f817
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-26 18:20:17 +0700

    AddExampleNamespace

---
+++ src/ch3-subchapter18/namespace-application/AppComputerScience.ts
@@ -0,0 +1,5 @@
+///<reference path = "IComputerScience.ts" />
+///<reference path = "OComputerScience.ts" />
+const stack = new ComputerScience.StackDataStructure();
+stack.createArray(["Maudy", "Ayunda"]);
+console.log(stack);
