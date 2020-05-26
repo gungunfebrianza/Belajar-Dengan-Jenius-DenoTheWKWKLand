02165f170da0a87e9524893bb4fc81064ee3b0e4
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-26 12:58:36 +0700

    ModifyTuple

--- src/chx-subchapter5/modify-tuple.ts
+++ src/chx-subchapter5/modify-tuple.ts
@@ -1,5 +1,7 @@
 var listUser: [number, string] = [1, "Maudy"];
 listUser[0] = 888;
-listUser[1] = "Ayunda"
+listUser[1] = "Ayunda";
 console.log(listUser[0]);
 console.log(listUser[1]);
+//888
+//Ayunda
