b9ed79036e70b8e7bc7001f6d9adff0c2282eb4f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-27 18:07:46 +0700

    UpdateCallback

--- src/ch5-subchapter5/callback.ts
+++ src/ch5-subchapter5/callback.ts
@@ -4,11 +4,19 @@ const getUser = (id: any, callback: any) => {
     name: "Gun Gun Febrianza",
   };
 
+  console.log("First Function Done!");
   setTimeout(() => {
     callback(user);
   }, 3000);
 };
 
 getUser(31, (userObject: any) => {
+  console.log("Then Executen Call back");
   console.log(userObject);
 });
+
+/* 
+  First Function Done!
+  Then Executen Call back
+  { id: 31, name: "Gun Gun Febrianza" } 
+  */
