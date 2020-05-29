3712184b4b79ceeaba4085169801eb7aebc8e528
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-30 04:38:08 +0700

    AddCheckPrimitiveType

---
+++ src/ch3-subchapter5/check-type/check-primitive.ts
@@ -0,0 +1,22 @@
+let stringPrimitive: String = "hi";
+let numberPrimitive: Number = 90;
+let boolPrimitive: Boolean = true;
+let nullPrimitive: null = null;
+let undefinedPrimitive: any;
+let bigIntPrimitive: BigInt = 2323n;
+
+console.log(typeof stringPrimitive);
+console.log(typeof numberPrimitive);
+console.log(typeof boolPrimitive);
+console.log(typeof nullPrimitive);
+console.log(typeof undefinedPrimitive);
+console.log(typeof bigIntPrimitive);
+
+/* 
+  string
+  number
+  boolean
+  object
+  undefined
+  bigint 
+  */
