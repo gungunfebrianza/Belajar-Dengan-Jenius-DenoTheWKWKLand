0f86e49bb64cb37df34aea6853314686d7845ad7
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-30 14:21:34 +0700

    AddObjectDestructure

---
+++ src/ch3-subchapter10/custom-object/object-destructure.ts
@@ -0,0 +1,9 @@
+const mod = {
+  firstname: "Maudy",
+  lastname: "Ayunda Faza",
+  age: 23,
+  haircolor: "black",
+};
+const { firstname, age } = mod;
+console.log(firstname, age);
+// Output Maudy 23
