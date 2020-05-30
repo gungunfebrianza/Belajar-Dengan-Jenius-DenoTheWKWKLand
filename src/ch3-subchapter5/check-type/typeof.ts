f415d0efa17da90b898f446086455456eba292ec
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-30 13:48:22 +0700

    AddTypeOf

---
+++ src/ch3-subchapter5/check-type/typeof.ts
@@ -0,0 +1,17 @@
+const map1 = new Map();
+const array = ["Hi", "Maudy"];
+const object1 = {};
+function reflect(param: any): any {
+  return param;
+}
+console.log(typeof map1);
+console.log(typeof array);
+console.log(typeof object1);
+console.log(typeof reflect(() => {}));
+
+/* 
+object
+object
+object
+function
+*/
