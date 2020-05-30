4ff856be59844d24c4f577144eaae7f08e36d558
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-30 14:24:33 +0700

    UpdateConfig

--- src/ch3-subchapter10/custom-object/object-prototype.js
+++ src/ch3-subchapter10/custom-object/object-prototype.js
@@ -1,4 +1,4 @@
-function Person(firstname, lastname, age, eyecolor) {
+/* function Person(firstname, lastname, age, eyecolor) {
   this.firstname = firstname;
   this.lastname = lastname;
   this.age = age;
@@ -10,3 +10,4 @@ Person.prototype.skill = "singing"; // -- Prototype
 var maudy = new Person("Maudy", "Ayunda Faza", 23, "Black");
 
 console.log(maudy.skill); //singing
+ */
