4c71f2c4a4c497e6eb1dfeda83483c79e2e06d0b
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-30 14:34:31 +0700

    AddTypeTemplate

---
+++ src/ch3-subchapter10/custom-object/type-template.ts
@@ -0,0 +1,11 @@
+const mod: {
+  firstname: string;
+  lastname: string;
+  age: number;
+  haircolor: string;
+} = {
+  firstname: "Maudy",
+  lastname: "Ayunda Faza",
+  age: 23,
+  haircolor: "black",
+};
