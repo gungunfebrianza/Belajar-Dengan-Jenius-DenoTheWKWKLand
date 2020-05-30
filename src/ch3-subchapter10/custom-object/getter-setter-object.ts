83fb23e4f10a7535cf236cedc875eda809fa7a21
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-30 14:22:39 +0700

    AddGetterSetter

---
+++ src/ch3-subchapter10/custom-object/getter-setter-object.ts
@@ -0,0 +1,15 @@
+var mod = {
+  firstname: "Maudy",
+  lastname: "Ayunda Faza",
+  age: 23,
+  haircolor: "black",
+  get getFirstName() {
+    return this.firstname;
+  },
+  set setFirstName(name: string) {
+    this.firstname = name;
+  },
+};
+console.log(mod.getFirstName); // Maudy
+mod.setFirstName = "Rindu";
+console.log(mod.getFirstName); // Rindu
