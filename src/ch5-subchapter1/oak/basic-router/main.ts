edf01b7d0902c57c61e51d16f716a3876316d269
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-05-31 21:16:16 +0700

    AddScript

--- src/ch5-subchapter1/oak/basic-router/main.ts
+++ src/ch5-subchapter1/oak/basic-router/main.ts
@@ -1,28 +1,12 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
-
-const books = new Map<string, any>();
-books.set("1", {
-  id: "1",
-  title: "The Hound of the Baskervilles",
-  author: "Conan Doyle, Arthur",
-});
-
-const router = new Router();
-router
-  .get("/", (context) => {
-    context.response.body = "Hello world!";
-  })
-  .get("/book", (context) => {
-    context.response.body = Array.from(books.values());
-  })
-  .get("/book/:id", (context) => {
-    if (context.params && context.params.id && books.has(context.params.id)) {
-      context.response.body = books.get(context.params.id);
-    }
-  });
+import { Application, send } from "https://deno.land/x/oak/mod.ts";
 
 const app = new Application();
-app.use(router.routes());
-app.use(router.allowedMethods());
+
+app.use(async (context) => {
+  await send(context, context.request.url.pathname, {
+    root: `${Deno.cwd()}/examples/static`,
+    index: "index.html",
+  });
+});
 
 await app.listen({ port: 8000 });
