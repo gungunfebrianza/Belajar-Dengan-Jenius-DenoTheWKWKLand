d312fefa3069c7656a40f23f892813190d5f06fc
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-02 01:55:35 +0700

    AddArrayLooping

---
+++ src/ch3-subchapter13/array/array-loopings/array-looping-es6.ts
@@ -0,0 +1,9 @@
+const mod5: string[] = ["Maudy", "Ayunda", "Faza"];
+mod5.forEach(function (item: string, index: number) {
+  console.log(item, index);
+});
+/* 
+    Maudy 0
+    Ayunda 1
+    Faza 2 
+    */
---
+++ src/ch3-subchapter13/array/array-loopings/array-looping.ts
@@ -0,0 +1,10 @@
+const mod6: string[] = ["Maudy", "Ayunda", "Faza"];
+var i: number = 0;
+for (i; i < mod6.length; i++) {
+  console.log(i + " " + mod6[i]);
+}
+/* 
+        0 Maudy
+        1 Ayunda
+        2 Faza 
+        */
