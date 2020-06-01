43362ded2c143b916495ead9c33b419c59ac5b06
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-02 02:33:21 +0700

    UpdateArrayMatrix

--- src/ch3-subchapter13/array/advanced-array/matrix.ts
+++ src/ch3-subchapter13/array/advanced-array/matrix.ts
@@ -5,3 +5,22 @@ const matrix: number[][] = [
 ];
 
 console.log(matrix[1][1]); //5
+
+var i: number = 0;
+for (i; i < matrix.length; i++) {
+  for (let j: number = 0; j < matrix[i].length; j++) {
+    console.log("Matrix[" + i + "][" + j + "] = " + matrix[j]);
+  }
+}
+
+/* 
+  Matrix[0][0] = 1,2,3
+  Matrix[0][1] = 4,5,6
+  Matrix[0][2] = 7,8,9
+  Matrix[1][0] = 1,2,3
+  Matrix[1][1] = 4,5,6
+  Matrix[1][2] = 7,8,9
+  Matrix[2][0] = 1,2,3
+  Matrix[2][1] = 4,5,6
+  Matrix[2][2] = 7,8,9 
+  */
