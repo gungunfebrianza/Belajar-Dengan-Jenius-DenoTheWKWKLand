0ab4a0484e475f6ceb72ebcdf70d7b269cb9be96
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-02 02:28:14 +0700

    AddLoopingArrayMulti

--- src/ch3-subchapter13/array/advanced-array/multidimensional-array.ts
+++ src/ch3-subchapter13/array/advanced-array/multidimensional-array.ts
@@ -1,14 +1,21 @@
-  const multi: number[][] = [[1, 2, 3], [23, 24, 25]];
-  console.log(multi[0][0]);
-  console.log(multi[0][1]);
-  console.log(multi[0][2]);
-  console.log(multi[1][0]);
-  console.log(multi[1][1]);
-  console.log(multi[1][2]);
-  /* 
+const multi: number[][] = [[1, 2, 3], [23, 24, 25]];
+console.log(multi[0][0]);
+console.log(multi[0][1]);
+console.log(multi[0][2]);
+console.log(multi[1][0]);
+console.log(multi[1][1]);
+console.log(multi[1][2]);
+/* 
   1
   2
   3
   23
   24
   25 */
+
+var i: number = 0;
+for (i; i < multi.length; i++) {
+  for (let j: number = 0; j < multi[i].length; j++) {
+    console.log(multi[i][j]);
+  }
+}
