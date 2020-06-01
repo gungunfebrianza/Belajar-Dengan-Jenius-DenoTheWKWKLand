203f6f511f2b9d10a22c7c34b88a4aa6fa3b76d0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-02 01:31:10 +0700

    AddArrayType

---
+++ src/ch3-subchapter13/array/array-type.ts
@@ -0,0 +1,6 @@
+var arrSym1: Symbol[] = [Symbol("maudy"), Symbol("ayunda")];
+var arrBig1: BigInt[] = [1n, 2n, 3n, 328932734623543264324n];
+var arrBol1: boolean[] = [false, true, false, true, false];
+var arrStr1: string[] = ["Gun Gun", "Febrianza", "Maudy"];
+var arrInt1: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 55, 33];
+console.log(arrInt1);
