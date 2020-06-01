25f6a3825dd64044eff90292662fd595dc9a0f2f
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-02 01:53:23 +0700

    AddMultiTypeArray

---
+++ src/ch3-subchapter13/array/advanced-array/multi-type-array.ts
@@ -0,0 +1,2 @@
+let multi1: (string | number)[] = [2, 2, "ayunda", 4, "Maudy"];
+console.log(multi1);
---
+++ src/ch3-subchapter13/array/advanced-array/multidimensional-array.ts
@@ -0,0 +1,14 @@
+  const multi: number[][] = [[1, 2, 3], [23, 24, 25]];
+  console.log(multi[0][0]);
+  console.log(multi[0][1]);
+  console.log(multi[0][2]);
+  console.log(multi[1][0]);
+  console.log(multi[1][1]);
+  console.log(multi[1][2]);
+  /* 
+  1
+  2
+  3
+  23
+  24
+  25 */
