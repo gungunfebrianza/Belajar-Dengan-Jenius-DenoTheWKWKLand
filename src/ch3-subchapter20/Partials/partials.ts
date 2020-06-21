ac4fbf2677fb63b15f00221f4ec74bd8d3c42c59
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-22 00:33:25 +0700

    AddPartials

---
+++ src/ch3-subchapter20/Partials/partials.ts
@@ -0,0 +1,20 @@
+interface Todo {
+  title: string;
+  description: string;
+}
+
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+  return { ...todo, ...fieldsToUpdate };
+}
+
+const todo1 = {
+  title: "organize desk",
+  description: "clear clutter",
+};
+
+const todo2 = updateTodo(todo1, {
+  description: "throw out trash",
+});
+
+console.log(todo1);
+console.log(todo2);
