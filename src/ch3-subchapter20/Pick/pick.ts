38c5d98f5dbb15d27cf953d59ed1c09517d011e0
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-22 00:39:09 +0700

    AddPick

---
+++ src/ch3-subchapter20/Pick/pick.ts
@@ -0,0 +1,14 @@
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = Pick<Todo, "title" | "completed">;
+
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+};
+
+console.log(todo);
