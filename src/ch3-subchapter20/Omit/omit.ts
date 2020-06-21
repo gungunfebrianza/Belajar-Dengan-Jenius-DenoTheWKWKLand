6ec0837420d4d850cecd93bb04b02df2d7facb90
gungunfebrianza <pusatrisetteknologi@gmail.com>
2020-06-22 00:18:22 +0700

    AddOmit

---
+++ src/ch3-subchapter20/Omit/omit.ts
@@ -0,0 +1,12 @@
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = Omit<Todo, "description">;
+
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+};
